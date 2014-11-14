e5b060cd9bea278a2d6aff5d2345eebb5eb23480
XMZALA <XMZALA@10.0.1.17>
2014-11-13 19:55:11 -0600

    11/13

--- Bank-Manager/Bank-Manager/Account.h
+++ Bank-Manager/Bank-Manager/Account.h
@@ -1,8 +1,32 @@
 #pragma once
-class Account
-{
-public:
-	Account();
-	~Account();
-};
+#include <iostream>
+#include <cstdlib>
+#include "Transaction.h"
+#include "Customer.h"
+#include "Date.h"
+#include <vector>
 
+using namespace std;
+
+class Account {
+public:
+    //constructors/destructor
+    Account(){}
+    Account(istream& input){ input >> balance; }
+    virtual ~Account(){}
+
+    //prototypes
+    void addTransaction(const Transaction&);
+    void sortTransactions();
+    void generateReport(ostream&);
+    
+    //simple one line accessors
+    vector<Transaction*> getTransactions(){ return transactions; }
+
+private:
+    double balance;
+    int accountID;
+    Date dateOpened;
+    vector<Transaction*> transactions;
+    vector<Customer*> customers;
+};
