ef0f582ba06592637eb0c4fa9855ef07553553c1
XMZALA <XMZALA@10.0.1.17>
2014-11-13 19:56:36 -0600

    11/13

--- Bank-Manager/Bank-Manager/Transaction.cpp
+++ Bank-Manager/Bank-Manager/Transaction.cpp
@@ -1,11 +1,44 @@
 #include "Transaction.h"
+#include "Date.h"
 
-
-Transaction::Transaction()
+//overload extraction operator
+istream& operator>>(istream& input, Transaction& transaction)
 {
+    input >> transaction.customerID >> transaction.accountID >> transaction.type;
+    input >> transaction.amount >> transaction.time;
+    getline(input, transaction.location);
+    return input;
 }
 
-
-Transaction::~Transaction()
+//overload insertion operator
+ostream& operator<<(ostream& output, const Transaction& transaction)
 {
+    output << transaction.time;
+    if (transaction.type == 'd' || transaction.type == 'D')
+    {
+        output << " Deposit, $" << transaction.amount << " from";
+    }
+    else if (transaction.type == 'w' || transaction.type == 'W')
+    {
+        output << " Withdrawal, $" << transaction.amount << " to";
+    }
+    output << transaction.location << ". ";
+    return output;
+}
+
+//overload less than operator
+bool Transaction::operator<(const Transaction& transaction) const
+{
+    if (time == transaction.time)
+    {
+        if (type == 'd' || type == 'D')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return (time < transaction.time);
 }
--- Bank-Manager/Bank-Manager/Transaction.h
+++ Bank-Manager/Bank-Manager/Transaction.h
@@ -1,8 +1,33 @@
 #pragma once
-class Transaction
-{
-public:
-	Transaction();
-	~Transaction();
-};
 
+#include <iostream>
+#include <cstdlib>
+#include "Date.h"
+
+using namespace std;
+
+class Transaction {    
+public:
+    //constructors/destructor
+    Transaction() {};
+    virtual ~Transaction(){}
+    
+    //prototypes
+    friend istream& operator>>(istream&, Transaction&);
+    friend ostream& operator<<(ostream&, const Transaction&);
+    bool operator<(const Transaction&) const;
+    
+    //simple one line accessors
+    Date getDate() { return time; }
+    char getType() { return type; }
+    string getLocation() { return location; }
+    double getAmount() { return amount; }
+
+private:
+    char type;
+    double amount;
+    Date time;
+    string location;
+    int accountID;
+    int customerID;
+};
