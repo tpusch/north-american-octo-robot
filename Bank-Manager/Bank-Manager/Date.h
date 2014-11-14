e3acaf9b3def164791911705edc666c1977282dd
XMZALA <XMZALA@10.0.1.17>
2014-11-13 19:56:18 -0600

    11/13

--- Bank-Manager/Bank-Manager/Date.h
+++ Bank-Manager/Bank-Manager/Date.h
@@ -1,8 +1,29 @@
 #pragma once
-class Date
-{
-public:
-	Date();
-	~Date();
-};
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+class Date {
+public:
+    //constructors/destructor
+    Date() {}
+    virtual ~Date(){}
+    
+    //prototypes
+    friend istream& operator>>(istream&, Date&);
+    friend ostream& operator<<(ostream&, const Date&);
+    bool operator<(const Date&) const;
+    bool operator==(const Date&) const;
+    
+    //simple one line accessors
+    int getMonth() { return month; }
+    int getDay() { return day; }
+    int getYear() { return year; }
+    char getSeparator1() { return separator1; }
+    char getSeparator2() { return separator2; }
+    
+private:
+    int month, day, year;
+    char separator1, separator2;
+};
