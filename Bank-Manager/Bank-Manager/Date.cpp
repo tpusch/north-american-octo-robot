25838c57b65873ef6c2addcb14c268177fd21898
XMZALA <XMZALA@10.0.1.17>
2014-11-13 19:55:52 -0600

    11/13

--- Bank-Manager/Bank-Manager/Date.cpp
+++ Bank-Manager/Bank-Manager/Date.cpp
@@ -1,11 +1,45 @@
 #include "Date.h"
 
-
-Date::Date()
+//overload extraction operator
+istream& operator>>(istream& input, Date& date)
 {
+    input >> date.month >> date.separator1 >> date.day >> date.separator2 >> date.year;
+    return input;
 }
 
-
-Date::~Date()
+//overload insertion operator
+ostream& operator<<(ostream& output, const Date& date)
 {
+    output << date.month << date.separator1 << date.day << date.separator2 << date.year;
+    return output;
+}
+
+//overload less than operator
+bool Date::operator<(const Date& date) const
+{
+    if (year == date.year)
+    {
+        if (month == date.month)
+        {
+            return (day < date.day);
+        }
+        else
+        {
+            return (month < date.month);
+        }
+    }
+    else
+    {
+        return (year < date.year);
+    }
+}
+
+//overload equality operator
+bool Date::operator==(const Date& date) const
+{
+    if (month == date.month && day == date.day && year == date.year)
+    {
+        return true;
+    }
+    return false;
 }
